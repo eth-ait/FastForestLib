7474627c9d1430efa7800e968e0986abb95be932
Benjamin Hepp <benjamin.hepp@inf.ethz.ch>
2016-03-09 13:52:36 +0100

    Updated eclipse cmake script

--- cpp/cmake_eclipse.sh
+++ cpp/cmake_eclipse.sh
@@ -1,8 +1,9 @@
-BUILD_TYPE=Debug
+#BUILD_TYPE=Debug
+BUILD_TYPE=RelWithDebInfo
 
 ECLIPSE_VERSION=4.2
 
-cmake ../ \
+cmake `dirname $0` \
     -DCMAKE_ECLIPSE_VERSION=$ECLIPSE_VERSION \
     -DCMAKE_CXX_COMPILER_ARG1=-std=c++11 \
     -DCMAKE_CXX_STANDARD=11 \
